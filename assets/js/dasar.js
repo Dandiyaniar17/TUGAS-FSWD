f1b40e75ec70ae738ef3e567ffa3a6ecf7ef54b2
Dandiyaniar17 <rdandiyaniar@gmail.com>
2023-09-12 10:16:26 +0700

    Tugas-fswd

---
+++ assets/js/dasar.js
@@ -0,0 +1,19 @@
+console.log("TUGAS7.1"); alert("TUGAS7.1");
+
+console.log("TUGAS7.1")
+alert("TUGAS7.1")
+var nama = "Arkatama";
+let id = 123;
+const alamat = "Malang"
+let n = 25;
+n=30
+let inf = (100/0);
+console.log(inf)
+console.log("Arkatama + 1")
+console.log("Multi Solusindo" + (2*5));
+let boyboyboy = null
+let name;
+console.log('name');
+let employee = Symbol('Dandi');
+console.log(employee);
+console.log(employee.description);
