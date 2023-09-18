0e640f4c17c46da8656d8a66049fe91ddff3eef6
Dandiyaniar17 <rdandiyaniar@gmail.com>
2023-09-18 15:11:32 +0700

    update

--- assets/js/produkarray.js
+++ assets/js/produkarray.js
@@ -33,15 +33,15 @@ let products = [{
     harga : "Rp 8.999.000"
 },
 {
-    nama:"Xiamoi",
-    gambar:"assets/images/xiaomi.png",
+    nama:"Xiaomi",
+    gambar:"../assets/images/Xiaomi.png",
     deskripsi:"Black Shark 5 Pro sendiri mengemas layar OLED 6,67 inci dengan resolusi FHD+ dan refresh rate 144Hz. Untuk spesifikasi, Snapdragon 8 Gen 1 dipilih sebagai dapur utama, ditemani oleh RAM 8/12/16GB, penyimpanan 128/256/512GB dan baterai 4,650mAh dengan fast-charging 120W",
     harga : "Rp 15.000.000"
 
 },
 {
     nama:"oppo",
-    gambar:"assets/images/oppo.png",
+    gambar:"../assets/images/oppo.png",
     deskripsi:"Dimensi dan bobot 163,7 x 73,9 x 8,5 mm, 218 gram Layar AMOLED 6,7 inci QHD Plus (3.216 x 1440 piksel), refresh rate 120 Hz, touch sampling rate 240 Hz, Gorilla Glass Victus Chipset Snapdragon 8 Gen 1 (8-core) GPU dan NPU Adreno 730 818 MHz",
     harga : "Rp 15.000.000"
 }
